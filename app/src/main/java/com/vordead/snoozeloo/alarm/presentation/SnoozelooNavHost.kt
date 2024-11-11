38ad3b626fe08b211f8453b49c0bc35dda6a6195
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-12 00:21:12 +0200

    created SnoozelooNavHost.kt, which sets up navigation between alarm list and alarm detail screens.

---
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/SnoozelooNavHost.kt
@@ -0,0 +1,25 @@
+package com.vordead.snoozeloo.alarm.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.vordead.snoozeloo.alarm.presentation.alarm_detail.navigation.alarmDetailDestination
+import com.vordead.snoozeloo.alarm.presentation.alarm_detail.navigation.navigateToAlarmDetail
+import com.vordead.snoozeloo.alarm.presentation.alarm_list.navigation.AlarmListNavigation
+import com.vordead.snoozeloo.alarm.presentation.alarm_list.navigation.alarmListDestination
+
+@Composable
+fun SnoozelooNavHost(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = AlarmListNavigation,
+        modifier = modifier
+    ) {
+        alarmListDestination(onNavigateToAlarmDetail = { alarmId ->
+            navController.navigateToAlarmDetail(alarmId)
+        })
+        alarmDetailDestination()
+    }
+}
