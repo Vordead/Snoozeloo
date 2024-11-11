870a2fb2e8273d1b3050e97f409c3f8705160a67
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-12 00:21:22 +0200

    created AlarmDetailDialog.kt empty skeleton

---
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/AlarmDetailDialog.kt
@@ -0,0 +1,12 @@
+package com.vordead.snoozeloo.alarm.presentation.alarm_detail
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AlarmDetailDialog(
+    alarmId: String,
+    modifier: Modifier = Modifier
+) {
+
+}
