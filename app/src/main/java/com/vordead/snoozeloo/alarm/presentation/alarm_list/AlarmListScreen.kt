6c9ec159bfd8789517ea60dca1f6ca890785dbea
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-12 00:20:49 +0200

    implemented AlarmListScreen UI and basic AlarmListViewModel logic

--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/AlarmListScreen.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/AlarmListScreen.kt
@@ -1,2 +1,25 @@
 package com.vordead.snoozeloo.alarm.presentation.alarm_list
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.vordead.snoozeloo.alarm.presentation.alarm_list.components.AlarmListItem
+
+@Composable
+fun AlarmListScreen(
+    state: AlarmListState,
+    onAction : (AlarmListAction) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn {
+        items(state.alarms) { alarm ->
+            AlarmListItem(
+                alarmListItemUi = alarm,
+                onAlarmSwitchClick = { isChecked ->
+                    onAction(AlarmListAction.onAlarmSwitchClick(alarm.id, isChecked))
+                }
+            )
+        }
+    }
+}
--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/AlarmListViewModel.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/AlarmListViewModel.kt
@@ -1,2 +1,29 @@
 package com.vordead.snoozeloo.alarm.presentation.alarm_list
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AlarmListViewModel @Inject constructor() : ViewModel() {
+    private val _alarmListState = MutableStateFlow(AlarmListState())
+    val state = _alarmListState
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5_000),
+            AlarmListState()
+        )
+
+
+    fun onAlarmSwitchClick(alarmId: String, isChecked: Boolean) {
+        viewModelScope.launch {
+
+        }
+    }
+
+}
