4d3cd7f5675e13fc7c65d2235e71b982ae5516c2
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-13 00:41:13 +0200

    fixed splash issue (action bar visible) and separated alarm list screen ui

--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/AlarmListScreen.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/AlarmListScreen.kt
@@ -1,10 +1,14 @@
 package com.vordead.snoozeloo.alarm.presentation.alarm_list
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.FabPosition
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,7 +17,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.vordead.snoozeloo.alarm.presentation.alarm_list.components.AddAlarmFab
 import com.vordead.snoozeloo.alarm.presentation.alarm_list.components.AlarmList
+import com.vordead.snoozeloo.alarm.presentation.alarm_list.components.AppBar
 import com.vordead.snoozeloo.alarm.presentation.models.AlarmListItemUi
 import com.vordead.snoozeloo.core.presentation.SnoozelooBackground
 
@@ -21,34 +27,39 @@ import com.vordead.snoozeloo.core.presentation.SnoozelooBackground
 fun AlarmListScreen(
     state: AlarmListState,
     onAction: (AlarmListAction) -> Unit,
-    modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .statusBarsPadding()
-    ) {
-        Text(
-            text = "Your Alarms",
-            modifier = Modifier
-                .padding(start = 16.dp, end = 16.dp, top = 16.dp),
-            style = MaterialTheme.typography.bodyLarge.copy(
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Medium,
-                color = Color(0xFF0D0F19),
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        floatingActionButton = {
+            AddAlarmFab(
+                onClick = { AlarmListAction.onCreateAlarmClick },
+                modifier = Modifier
+                    .navigationBarsPadding()
+                    .padding(bottom = 30.dp),
             )
-        )
-        AlarmList(
-            alarms = state.alarms,
-            onAlarmClick = { alarm ->
-                onAction(AlarmListAction.onAlarmClick(alarm.id))
-            },
-            onAlarmSwitchClick = { alarm, isChecked ->
-                onAction(AlarmListAction.onAlarmSwitchClick(alarm.id, isChecked))
-            },
-        )
-    }
-
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .statusBarsPadding()
+                    .consumeWindowInsets(paddingValues)
+            ) {
+                AppBar(title = "Your Alarms")
+                AlarmList(
+                    alarms = state.alarms,
+                    onAlarmClick = { alarm ->
+                        onAction(AlarmListAction.onAlarmClick(alarm.id))
+                    },
+                    onAlarmSwitchClick = { alarm, isChecked ->
+                        onAction(AlarmListAction.onAlarmSwitchClick(alarm.id, isChecked))
+                    },
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+    )
 }
 
 @Preview
--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/components/AlarmList.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/components/AlarmList.kt
@@ -2,13 +2,11 @@ package com.vordead.snoozeloo.alarm.presentation.alarm_list.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.vordead.snoozeloo.alarm.presentation.alarm_list.AlarmListAction
 import com.vordead.snoozeloo.alarm.presentation.models.AlarmListItemUi
 
 @Composable
@@ -19,8 +17,8 @@ fun AlarmList(
     modifier: Modifier = Modifier
 ) {
     LazyColumn(
-        modifier = modifier.fillMaxSize(),
-        contentPadding = PaddingValues(16.dp),
+        modifier = modifier,
+        contentPadding = PaddingValues(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 48.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         items(alarms) { alarm ->
