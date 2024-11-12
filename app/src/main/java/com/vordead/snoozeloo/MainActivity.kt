3aeaccd7949e253ed01a2cadf212de52dc206ce0
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-12 21:31:14 +0200

    added splash api and splash screen

--- app/src/main/java/com/vordead/snoozeloo/MainActivity.kt
+++ app/src/main/java/com/vordead/snoozeloo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.vordead.snoozeloo.ui.SnoozelooApp
 import com.vordead.snoozeloo.ui.theme.SnoozelooTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,6 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             SnoozelooTheme {
                 SnoozelooApp()
