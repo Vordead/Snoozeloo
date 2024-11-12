e78a401451adaf7d4ae4ddc41e40d25502201514
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-12 19:22:09 +0200

    added onAlarmClick action to AlarmListAction to handle alarm item clicks.

--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/AlarmListAction.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/AlarmListAction.kt
@@ -2,5 +2,6 @@ package com.vordead.snoozeloo.alarm.presentation.alarm_list
 
 interface AlarmListAction {
     data object onCreateAlarmClick : AlarmListAction
+    data class onAlarmClick(val alarmId: String) : AlarmListAction
     data class onAlarmSwitchClick(val alarmId: String, val isChecked: Boolean) : AlarmListAction
 }
