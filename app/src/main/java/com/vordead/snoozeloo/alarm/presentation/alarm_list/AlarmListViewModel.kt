347fd3a4bcab858a396bd890ce5cc2991da0fdca
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-12 19:42:25 +0200

    implemented AlarmListViewModel logic to load and display a sample alarm.

--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/AlarmListViewModel.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/AlarmListViewModel.kt
@@ -2,10 +2,13 @@ package com.vordead.snoozeloo.alarm.presentation.alarm_list
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.vordead.snoozeloo.alarm.presentation.models.AlarmListItemUi
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -13,6 +16,7 @@ import javax.inject.Inject
 class AlarmListViewModel @Inject constructor() : ViewModel() {
     private val _alarmListState = MutableStateFlow(AlarmListState())
     val state = _alarmListState
+        .onStart { loadAlarms() }
         .stateIn(
             viewModelScope,
             SharingStarted.WhileSubscribed(5_000),
@@ -23,6 +27,25 @@ class AlarmListViewModel @Inject constructor() : ViewModel() {
     fun onAlarmSwitchClick(alarmId: String, isChecked: Boolean) {
         viewModelScope.launch {
 
+        }
+    }
+
+    private fun loadAlarms() {
+        viewModelScope.launch {
+            _alarmListState.update {
+                it.copy(
+                    alarms = listOf(
+                        AlarmListItemUi(
+                            id = "1",
+                            title = "Alarm 1",
+                            time = "10:00",
+                            period = "AM",
+                            remainingTime = "30min",
+                            isEnabled = false
+                        )
+                    )
+                )
+            }
         }
     }
 
