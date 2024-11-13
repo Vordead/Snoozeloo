ee02c5de6744ca4c9c3c35308524a113c55cef0a
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-13 23:55:35 +0200

    created alarm detail app bar

--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/AlarmDetailDialog.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/AlarmDetailDialog.kt
@@ -1,12 +1,31 @@
 package com.vordead.snoozeloo.alarm.presentation.alarm_detail
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vordead.snoozeloo.alarm.presentation.alarm_detail.components.AlarmDetailAppBar
+import com.vordead.snoozeloo.core.presentation.SnoozelooBackground
 
 @Composable
 fun AlarmDetailDialog(
-    alarmId: String,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    alarmId: String? = null,
 ) {
+    Column(modifier = modifier) {
+        AlarmDetailAppBar(
+            onBackClick = {},
+            onSaveClick = {},
+            modifier = Modifier
+        )
+    }
+}
 
+@Preview
+@Composable
+private fun AlarmDetailDialogPreview() {
+    SnoozelooBackground {
+        AlarmDetailDialog(alarmId = "1")
+    }
 }
