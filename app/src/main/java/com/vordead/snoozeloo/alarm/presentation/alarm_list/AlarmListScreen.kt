12a61c8b8a8c259809130fc8136f571fc1a4fee2
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-13 23:55:58 +0200

    added gradient effect on top of alarms list

--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/AlarmListScreen.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/AlarmListScreen.kt
@@ -1,16 +1,24 @@
 package com.vordead.snoozeloo.alarm.presentation.alarm_list
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.FabPosition
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,16 +62,20 @@ fun AlarmListScreen(
                             shape = CircleShape
                         )
                 )
-                AlarmList(
-                    alarms = state.alarms,
-                    onAlarmClick = { alarm ->
-                        onAction(AlarmListAction.onAlarmClick(alarm.id))
-                    },
-                    onAlarmSwitchClick = { alarm, isChecked ->
-                        onAction(AlarmListAction.onAlarmSwitchClick(alarm.id, isChecked))
-                    },
-                    modifier = Modifier.fillMaxSize()
-                )
+                Box {
+                    AlarmList(
+                        alarms = state.alarms,
+                        onAlarmClick = { alarm ->
+                            onAction(AlarmListAction.onAlarmClick(alarm.id))
+                        },
+                        onAlarmSwitchClick = { alarm, isChecked ->
+                            onAction(AlarmListAction.onAlarmSwitchClick(alarm.id, isChecked))
+                        },
+                        modifier = Modifier.fillMaxSize()
+                    )
+                    LandingHeader()
+                }
+
             }
         }
     )
@@ -88,5 +100,33 @@ private fun AlarmListScreenPreview() {
             ),
             onAction = {}
         )
+    }
+}
+
+@Composable
+fun LandingHeader() {
+    Box(
+        modifier = Modifier
+            .systemBarsPadding()
+            .fillMaxWidth()
+            .height(40.dp)
+            .background(
+                brush = Brush.linearGradient(
+                    0.0f to MaterialTheme.colorScheme.background,
+                    1.0f to Color.Transparent,
+                    start = Offset(0.0f, 0.0f),
+                    end = Offset(0.0f, 100.0f)
+                )
+            )
+    ) {
+        // Your content here
+    }
+}
+
+@Preview
+@Composable
+private fun LandingPreview() {
+    SnoozelooBackground {
+        LandingHeader()
     }
 }
