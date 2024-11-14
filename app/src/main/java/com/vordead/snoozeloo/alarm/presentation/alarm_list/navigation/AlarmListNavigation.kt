9809a9f65397308acc97bcdb12090354426cc236
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-15 01:30:18 +0200

    added navigation to alarm detail screen from alarm list screen and handled alarm switch click.

--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/navigation/AlarmListNavigation.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/navigation/AlarmListNavigation.kt
@@ -5,6 +5,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.vordead.snoozeloo.alarm.presentation.alarm_detail.AlarmDetailAction
 import com.vordead.snoozeloo.alarm.presentation.alarm_list.AlarmListAction
 import com.vordead.snoozeloo.alarm.presentation.alarm_list.AlarmListScreen
 import com.vordead.snoozeloo.alarm.presentation.alarm_list.AlarmListViewModel
@@ -25,6 +26,7 @@ fun NavGraphBuilder.alarmListDestination(
                 when(it) {
                     is AlarmListAction.onAlarmSwitchClick -> alarmListViewModel.onAlarmSwitchClick(it.alarmId, it.isChecked)
                     AlarmListAction.onCreateAlarmClick -> onNavigateToAlarmDetail("")
+                    is AlarmListAction.onAlarmClick -> TODO()
                 }
             }
         )
