bb289a3024d52571acc90b94f32962a2c1c0993f
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-15 01:30:07 +0200

    updated AlarmListAction interface to a sealed interface.

--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/AlarmListAction.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/AlarmListAction.kt
@@ -1,6 +1,6 @@
 package com.vordead.snoozeloo.alarm.presentation.alarm_list
 
-interface AlarmListAction {
+sealed interface AlarmListAction {
     data object onCreateAlarmClick : AlarmListAction
     data class onAlarmClick(val alarmId: String) : AlarmListAction
     data class onAlarmSwitchClick(val alarmId: String, val isChecked: Boolean) : AlarmListAction
