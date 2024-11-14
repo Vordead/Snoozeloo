2e1971dbfbc85b0f106db4682461de8a637f1a90
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-15 01:30:34 +0200

    added AlarmDetailViewModel to manage the UI state of the Alarm Detail screen.

---
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/AlarmDetailViewModel.kt
@@ -0,0 +1,19 @@
+package com.vordead.snoozeloo.alarm.presentation.alarm_detail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class AlarmDetailViewModel @Inject constructor(): ViewModel() {
+    private val _uiState = MutableStateFlow(AlarmDetailState())
+    val uiState = _uiState.stateIn(
+        viewModelScope,
+        initialValue = AlarmDetailState(),
+        started = SharingStarted.WhileSubscribed(5_000)
+    )
+}
