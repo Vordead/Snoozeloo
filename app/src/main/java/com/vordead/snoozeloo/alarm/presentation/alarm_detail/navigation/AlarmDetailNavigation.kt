7b83f44f31b9753bd50ff01ee7d241dacb3855ae
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-15 01:30:42 +0200

    Refactor: Changed AlarmDetailDialog navigation to composable
    Updated AlarmDetailDialog navigation to use composable instead of dialog.
    This allows for the AlarmDetailDialog to be displayed as a full-screen composable instead of a dialog.
    Also made the id parameter of AlarmDetail nullable to allow for creating new alarms.

--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/navigation/AlarmDetailNavigation.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/navigation/AlarmDetailNavigation.kt
@@ -2,19 +2,20 @@ package com.vordead.snoozeloo.alarm.presentation.alarm_detail.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.dialog
 import com.vordead.snoozeloo.alarm.presentation.alarm_detail.AlarmDetailDialog
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class AlarmDetail(val id: String)
+data class AlarmDetail(val id: String? = null)
 
 fun NavController.navigateToAlarmDetail(id: String) = navigate(AlarmDetail(id))
 
 
 fun NavGraphBuilder.alarmDetailDestination(){
-    dialog<AlarmDetail> { backStackEntry ->
-        val alarmId = backStackEntry.arguments?.getString("id") ?: return@dialog
+    composable<AlarmDetail> { backStackEntry ->
+        val alarmId = backStackEntry.arguments?.getString("id")
         AlarmDetailDialog(alarmId = alarmId)
     }
 }
