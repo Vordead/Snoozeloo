0b27ebbf0fff4ff7816a079bbda87af196cd0de3
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-16 22:56:22 +0200

    added request focus on textfield on first time entering screen

--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/components/AlarmFieldChangeDialog.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/components/AlarmFieldChangeDialog.kt
@@ -13,13 +13,15 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,7 +29,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
-import androidx.compose.ui.window.SecureFlagPolicy
 import com.vordead.snoozeloo.core.presentation.SnoozelooBackground
 
 @Composable
@@ -38,8 +39,13 @@ fun AlarmFieldChangeDialog(
     fieldText: String = "",
 ) {
     var text by rememberSaveable { mutableStateOf(fieldText) }
+    val focusRequester = FocusRequester()
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+
     Dialog(
-        onDismissRequest = {onDismiss() },
+        onDismissRequest = { onDismiss() },
         properties = DialogProperties(
             dismissOnBackPress = true,
             dismissOnClickOutside = true,
@@ -66,7 +72,9 @@ fun AlarmFieldChangeDialog(
                 OutlinedTextField(
                     text,
                     onValueChange = { text = it },
-                    modifier = Modifier.padding(top = 10.dp),
+                    modifier = Modifier
+                        .padding(top = 10.dp)
+                        .focusRequester(focusRequester),
                     singleLine = true,
                     textStyle = MaterialTheme.typography.bodyMedium.copy(
                         fontSize = 14.sp,
