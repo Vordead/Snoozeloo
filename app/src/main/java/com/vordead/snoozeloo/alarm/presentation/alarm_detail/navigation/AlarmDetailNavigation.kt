a99d58c0a4cd8a39a65144ddd7460a475e9da198
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-16 21:06:23 +0200

    Refactor: Migrate AlarmDetailDialog to AlarmDetailScreen
    Migrated the AlarmDetailDialog to a screen-based implementation using AlarmDetailScreen.
    Added navigation transitions for a smoother user experience.
    Integrated AlarmDetailViewModel to manage screen state and handle user actions.

--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/navigation/AlarmDetailNavigation.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/navigation/AlarmDetailNavigation.kt
@@ -1,11 +1,16 @@
 package com.vordead.snoozeloo.alarm.presentation.alarm_detail.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.Down
+import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.Up
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.dialog
 import androidx.navigation.toRoute
-import com.vordead.snoozeloo.alarm.presentation.alarm_detail.AlarmDetailDialog
+import com.vordead.snoozeloo.alarm.presentation.alarm_detail.AlarmDetailAction
+import com.vordead.snoozeloo.alarm.presentation.alarm_detail.AlarmDetailScreen
+import com.vordead.snoozeloo.alarm.presentation.alarm_detail.AlarmDetailViewModel
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -14,9 +19,43 @@ data class AlarmDetail(val id: String? = null)
 fun NavController.navigateToAlarmDetail(id: String) = navigate(AlarmDetail(id))
 
 
-fun NavGraphBuilder.alarmDetailDestination(){
-    composable<AlarmDetail> { backStackEntry ->
+fun NavGraphBuilder.alarmDetailDestination(
+    onNavigateBack: () -> Unit,
+    onNavigateToAlarmDialog : () -> Unit,
+) {
+    composable<AlarmDetail>(
+        enterTransition = {
+            slideIntoContainer(Up)
+        },
+        exitTransition = {
+            slideOutOfContainer(Down)
+        }
+    ) { backStackEntry ->
         val args = backStackEntry.toRoute<AlarmDetail>()
-        AlarmDetailDialog(alarmId = args.id)
+        val vm: AlarmDetailViewModel = hiltViewModel()
+        val uiState = vm.uiState.collectAsStateWithLifecycle()
+        AlarmDetailScreen(
+            alarmId = args.id,
+            state = uiState.value,
+            onAction = {
+                when (it) {
+                    is AlarmDetailAction.onSaveClick -> {
+                        onNavigateBack()
+                    }
+
+                    is AlarmDetailAction.onBackClick -> {
+                        onNavigateBack()
+                    }
+
+                    is AlarmDetailAction.onAlarmNameClick -> {
+                        // Handle alarm name click
+                    }
+
+                    is AlarmDetailAction.onTimeChange -> {
+                        // Handle time change
+                    }
+                }
+            }
+        )
     }
 }
