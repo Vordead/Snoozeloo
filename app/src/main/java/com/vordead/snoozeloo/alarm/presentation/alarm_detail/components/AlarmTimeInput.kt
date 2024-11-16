0832a85a87f83845b9f1b1ae7f7d503aa75f3692
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-16 17:31:27 +0200

    feat: display time left until alarm in AlarmTimeInput
    This commit introduces a new feature to the AlarmTimeInput component. It now displays the time left until the alarm is triggered, provided both hour and minute fields are filled. The time left is dynamically calculated and displayed below the time input fields. This is achieved using animations for a smooth user experience.

--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/components/AlarmTimeInput.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/components/AlarmTimeInput.kt
@@ -1,6 +1,11 @@
 package com.vordead.snoozeloo.alarm.presentation.alarm_detail.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,7 +17,13 @@ import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -33,34 +44,57 @@ fun AlarmTimeInput(
     val hourTextInputState = rememberTextFieldState("")
     val minuteTextInputState = rememberTextFieldState("")
 
+    var shouldShowTimeLeft by remember { mutableStateOf(false) }
+
+    LaunchedEffect(hourTextInputState.text, minuteTextInputState.text) {
+        shouldShowTimeLeft =
+            hourTextInputState.text.isNotEmpty() && minuteTextInputState.text.isNotEmpty()
+    }
+
     Card(
         modifier = modifier,
         shape = RoundedCornerShape(10.dp),
         colors = CardDefaults.cardColors(containerColor = Color.White)
     ) {
-        Row(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(24.dp),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
+        Column(
+            modifier = Modifier.animateContentSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            TimeInputField(
-                state = hourTextInputState,
-                inputTransformation = InputTransformation.hourInput(),
-                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
-            )
-            Icon(
-                imageVector = ImageVector.vectorResource(R.drawable.ic_colon),
-                contentDescription = "Colon",
-                tint = Color(0xFF858585),
-                modifier = Modifier.padding(horizontal = 10.dp)
-            )
-            TimeInputField(
-                state = minuteTextInputState,
-                inputTransformation = InputTransformation.minuteInput(),
-                modifier = Modifier
-            )
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(start = 24.dp, end = 24.dp, top = 24.dp, bottom = 24.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TimeInputField(
+                    state = hourTextInputState,
+                    inputTransformation = InputTransformation.hourInput(),
+                    keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+                )
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_colon),
+                    contentDescription = "Colon",
+                    tint = Color(0xFF858585),
+                    modifier = Modifier.padding(horizontal = 10.dp)
+                )
+                TimeInputField(
+                    state = minuteTextInputState,
+                    inputTransformation = InputTransformation.minuteInput(),
+                    modifier = Modifier
+                )
+            }
+
+            AnimatedVisibility(
+                visible = shouldShowTimeLeft,
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+                Text(
+                    "Alarm in 8h 13min",
+                    modifier = Modifier.padding(bottom = 24.dp)
+                )
+            }
         }
     }
 }
