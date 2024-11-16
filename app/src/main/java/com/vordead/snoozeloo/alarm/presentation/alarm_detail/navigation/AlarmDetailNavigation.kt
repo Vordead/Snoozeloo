70a6fc7486a0884b28229a04261c87daf4f8d616
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-16 17:35:41 +0200

    refactor: updated alarm detail navigation to use toRoute
    Updated the alarm detail navigation to utilize the `toRoute` function for retrieving arguments, enhancing type safety and code clarity.

--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/navigation/AlarmDetailNavigation.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/navigation/AlarmDetailNavigation.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.dialog
+import androidx.navigation.toRoute
 import com.vordead.snoozeloo.alarm.presentation.alarm_detail.AlarmDetailDialog
 import kotlinx.serialization.Serializable
 
@@ -15,7 +16,7 @@ fun NavController.navigateToAlarmDetail(id: String) = navigate(AlarmDetail(id))
 
 fun NavGraphBuilder.alarmDetailDestination(){
     composable<AlarmDetail> { backStackEntry ->
-        val alarmId = backStackEntry.arguments?.getString("id")
-        AlarmDetailDialog(alarmId = alarmId)
+        val args = backStackEntry.toRoute<AlarmDetail>()
+        AlarmDetailDialog(alarmId = args.id)
     }
 }
