191c413eb1d65c2c9171be962794beb42c7e33c9
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-16 17:34:19 +0200

    feat: added AlarmSetting composable
    Added a new composable function called `AlarmSetting` that provides a reusable card-like UI element for displaying alarm settings.
    
    This composable includes:
    - Customizable title, trailing content, and sub-content slots.
    - Clickable card behavior (optional).
    - Styling with rounded corners, padding, and typography.

---
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/components/AlarmSetting.kt
@@ -0,0 +1,84 @@
+package com.vordead.snoozeloo.alarm.presentation.alarm_detail.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.vordead.snoozeloo.core.presentation.SnoozelooBackground
+import com.vordead.snoozeloo.ui.theme.SnoozelooTheme
+
+
+@Composable
+fun AlarmSetting(
+    title: String,
+    modifier: Modifier = Modifier,
+    onClick: (() -> Unit)? = null,
+    trailingContent: (@Composable () -> Unit)? = null,
+    subContent: (@Composable () -> Unit)? = null
+) {
+    Card(
+        modifier = modifier
+            .clip(RoundedCornerShape(10.dp))
+            .clickable(enabled = onClick != null, onClick = { onClick?.invoke() }),
+        shape = RoundedCornerShape(10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        )
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    title,
+                    modifier = modifier,
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight(600),
+                        color = Color(0xFF0D0F19),
+                    )
+                )
+                trailingContent?.invoke()
+            }
+            subContent?.let{
+                Spacer(Modifier.padding(top = 10.dp))
+                subContent()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AlarmSettingPreview() {
+    SnoozelooBackground {
+
+    }
+    SnoozelooTheme {
+        AlarmSetting(title = "Alarm Setting", trailingContent = {
+            Text("Trailing Content", modifier = Modifier)
+        }, subContent = {
+            Text("Sub Content")
+        })
+    }
+}
