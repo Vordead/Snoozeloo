1bc00df6eedc9ce8c5be5f6551a72c1bdd36527f
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-16 17:35:03 +0200

    feat: implemented basic AlarmDetail UI
    Implemented the basic UI for the AlarmDetail screen, including:
    
    - An app bar with back and save buttons.
    - A section for setting the alarm time.
    - A section for setting the alarm name with a trailing text displaying the current name.
    - Updated padding and spacing for improved layout.

--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/AlarmDetailDialog.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/AlarmDetailDialog.kt
@@ -1,17 +1,23 @@
 package com.vordead.snoozeloo.alarm.presentation.alarm_detail
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vordead.snoozeloo.alarm.presentation.alarm_detail.components.AlarmDetailAppBar
+import com.vordead.snoozeloo.alarm.presentation.alarm_detail.components.AlarmSetting
 import com.vordead.snoozeloo.alarm.presentation.alarm_detail.components.AlarmTimeInput
 import com.vordead.snoozeloo.core.presentation.SnoozelooBackground
 
@@ -22,22 +28,40 @@ fun AlarmDetailDialog(
     alarmId: String? = null,
 ) {
     Scaffold(topBar = {
-        CenterAlignedTopAppBar({
-            AlarmDetailAppBar(
-                onBackClick = {},
-                onSaveClick = {},
-                modifier = Modifier
+        CenterAlignedTopAppBar(
+            title = {
+                AlarmDetailAppBar(
+                    onBackClick = {},
+                    onSaveClick = {},
+                    modifier = Modifier
+                )
+            },
+            modifier = Modifier.padding(start = 10.dp, end = 10.dp, top = 16.dp),
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.background
             )
-        })
+        )
+
 
     }) { innerPadding ->
         Column(
             modifier = modifier
                 .padding(innerPadding)
-                .padding(horizontal = 16.dp)
+                .padding(horizontal = 16.dp, vertical = 24.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-
             AlarmTimeInput { _, _ -> }
+            AlarmSetting(
+                title = "Alarm Name",
+                onClick = {
+
+                },
+                trailingContent = {
+                    Text("Work", style = MaterialTheme.typography.bodyMedium)
+                }
+            )
+
+
         }
     }
 
