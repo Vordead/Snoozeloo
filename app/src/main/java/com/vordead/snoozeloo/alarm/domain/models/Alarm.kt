022da8250d926b8f2a027f69b759049e88d43f38
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-17 14:35:45 +0200

    added Alarm domain layer

---
+++ app/src/main/java/com/vordead/snoozeloo/alarm/domain/models/Alarm.kt
@@ -0,0 +1,12 @@
+package com.vordead.snoozeloo.alarm.domain.models
+
+data class Alarm(
+    val id: Int = 0,
+    val time: String, // Format "HH:mm"
+    val isEnabled: Boolean,
+    val repeatDays: List<String>,
+    val alarmName: String? = null,
+    val ringtone: String? = null,
+    val volume: Int = 50,
+    val vibrate: Boolean = true
+)
