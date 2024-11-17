0fa9beb7212371b455010ca357fc8c7f042c833d
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-17 14:34:48 +0200

    feat: add AlarmDataSource interface
    Added an interface `AlarmDataSource` to define the contract for accessing and manipulating alarm data.
    It includes methods for inserting, updating, deleting, and retrieving alarms.

--- app/src/main/java/com/vordead/snoozeloo/alarm/domain/AlarmDataSource.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/domain/AlarmDataSource.kt
@@ -1,2 +1,13 @@
 package com.vordead.snoozeloo.alarm.domain
 
+import com.vordead.snoozeloo.alarm.data.local.AlarmEntity
+import kotlinx.coroutines.flow.Flow
+
+interface AlarmDataSource {
+    suspend fun insert(alarmEntity: AlarmEntity)
+    suspend fun update(alarmEntity: AlarmEntity)
+    suspend fun delete(alarmEntity: AlarmEntity)
+    suspend fun getAlarmById(id: Int): AlarmEntity?
+    fun getAllAlarms(): Flow<List<AlarmEntity>>
+    suspend fun deleteById(id: Int)
+}
