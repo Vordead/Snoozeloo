fd9a0509841491319d6679210f9f3f663817195d
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-18 21:37:57 +0200

    merged @Insert and @Update in dao to @Upsert

--- app/src/main/java/com/vordead/snoozeloo/alarm/data/local/AlarmDao.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/data/local/AlarmDao.kt
@@ -2,19 +2,15 @@ package com.vordead.snoozeloo.alarm.data.local
 
 import androidx.room.Dao
 import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Update
+import androidx.room.Upsert
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface AlarmDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(alarmEntity: AlarmEntity)
 
-    @Update
-    suspend fun update(alarmEntity: AlarmEntity)
+    @Upsert
+    suspend fun upsert(alarmEntity: AlarmEntity)
 
     @Delete
     suspend fun delete(alarmEntity: AlarmEntity)
