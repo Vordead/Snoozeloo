241f7897db14dc908129290fe733f57b4e729e0b
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-18 22:13:17 +0200

    successfully made us of the room database in the project

--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/AlarmDetailViewModel.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_detail/AlarmDetailViewModel.kt
@@ -2,6 +2,9 @@ package com.vordead.snoozeloo.alarm.presentation.alarm_detail
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.vordead.snoozeloo.alarm.data.local.LocalAlarmDataSource
+import com.vordead.snoozeloo.alarm.domain.models.Alarm
+import com.vordead.snoozeloo.alarm.presentation.models.AlarmUi
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
@@ -11,13 +14,30 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AlarmDetailViewModel @Inject constructor() : ViewModel() {
+class AlarmDetailViewModel @Inject constructor(
+    private val localAlarmDataSource: LocalAlarmDataSource
+) : ViewModel() {
     private val _uiState = MutableStateFlow(AlarmDetailState())
     val uiState = _uiState.stateIn(
         viewModelScope,
         initialValue = AlarmDetailState(),
         started = SharingStarted.WhileSubscribed(5_000)
     )
+
+    init {
+        viewModelScope.launch {
+            createOrUpdateAlarm(
+                AlarmUi(
+                    id = "1",
+                    title = "Alarm",
+                    time = "12:00",
+                    period = "Mon, Tue, Wed, Thu, Fri",
+                    isEnabled = true,
+                    remainingTime = "Remaining time"
+                )
+            )
+        }
+    }
 
     fun onAlarmNameChange(alarmName: String) {
         viewModelScope.launch {
@@ -30,7 +50,25 @@ class AlarmDetailViewModel @Inject constructor() : ViewModel() {
         }
     }
 
-    fun showAlarmDialog(shouldShow : Boolean) {
+    fun createOrUpdateAlarm(alarm: AlarmUi) {
+        viewModelScope.launch {
+            localAlarmDataSource.upsertAlarm(
+                Alarm(
+                    id = alarm.id.toInt(),
+                    alarmName = "alarm.title",
+                    time = "alarm.time",
+                    repeatDays = alarm.period.split(", "),
+                    isEnabled = alarm.isEnabled
+                )
+            )
+        }
+    }
+
+
+    fun showAlarmDialog(shouldShow: Boolean) {
+        val ss = "hello"
+        val res = Result.success(ss)
+        res.onSuccess { value -> println(value) }
         viewModelScope.launch {
             _uiState.update {
                 it.copy(showAlarmNameDialog = shouldShow)
--- app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/AlarmListViewModel.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/presentation/alarm_list/AlarmListViewModel.kt
@@ -2,98 +2,50 @@ package com.vordead.snoozeloo.alarm.presentation.alarm_list
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.vordead.snoozeloo.alarm.presentation.models.AlarmListItemUi
+import com.vordead.snoozeloo.alarm.data.local.LocalAlarmDataSource
+import com.vordead.snoozeloo.alarm.presentation.models.toAlarmUi
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AlarmListViewModel @Inject constructor() : ViewModel() {
+class AlarmListViewModel @Inject constructor(
+    private val localAlarmDataSource: LocalAlarmDataSource
+) : ViewModel() {
+
     private val _alarmListState = MutableStateFlow(AlarmListState())
+    @OptIn(ExperimentalCoroutinesApi::class)
     val state = _alarmListState
-        .onStart { loadAlarms() }
+        .flatMapLatest {
+            localAlarmDataSource.getAllAlarms()
+                .map { alarms ->
+                    AlarmListState(alarms = alarms.map { it.toAlarmUi() })
+                }
+        }
         .stateIn(
             viewModelScope,
             SharingStarted.WhileSubscribed(5_000),
             AlarmListState()
         )
 
-
     fun onAlarmSwitchClick(alarmId: String, isChecked: Boolean) {
         viewModelScope.launch {
-            _alarmListState.update {
-                it.copy(
-                    alarms = it.alarms.map { alarm ->
+            _alarmListState.update { currentState ->
+                currentState.copy(
+                    alarms = currentState.alarms.map { alarm ->
                         if (alarm.id == alarmId) {
                             alarm.copy(isEnabled = isChecked)
                         } else {
                             alarm
                         }
                     }
-                )
-            }
-        }
-    }
-
-    private fun loadAlarms() {
-        viewModelScope.launch {
-            _alarmListState.update {
-                it.copy(
-                    alarms = listOf(
-                        AlarmListItemUi(
-                            id = "1",
-                            title = "Alarm 1",
-                            time = "10:00",
-                            period = "AM",
-                            remainingTime = "30min",
-                            isEnabled = false
-                        ),
-                        AlarmListItemUi(
-                            id = "2",
-                            title = "Alarm 2",
-                            time = "11:00",
-                            period = "AM",
-                            remainingTime = "1h",
-                            isEnabled = true
-                        ),
-                        AlarmListItemUi(
-                            id = "3",
-                            title = "Alarm 3",
-                            time = "12:00",
-                            period = "PM",
-                            remainingTime = "2h",
-                            isEnabled = false
-                        ),
-                        AlarmListItemUi(
-                            id = "4",
-                            title = "Alarm 4",
-                            time = "01:00",
-                            period = "PM",
-                            remainingTime = "3h",
-                            isEnabled = true
-                        ),
-                        AlarmListItemUi(
-                            id = "5",
-                            title = "Alarm 5",
-                            time = "02:00",
-                            period = "PM",
-                            remainingTime = "4h",
-                            isEnabled = false
-                        ),
-                        AlarmListItemUi(
-                            id = "6",
-                            title = "Alarm 6",
-                            time = "03:00",
-                            period = "PM",
-                            remainingTime = "5h",
-                            isEnabled = true
-                        ),
-                    )
                 )
             }
         }
