7004596caf62d2531260ef8b9259410e3ca3b12c
Mohamad Hamade <mohamadhamade30@gmail.com>
2024-11-18 22:12:39 +0200

    added di for database, dao and alarm datasource

--- app/src/main/java/com/vordead/snoozeloo/alarm/di/AlarmModule.kt
+++ app/src/main/java/com/vordead/snoozeloo/alarm/di/AlarmModule.kt
@@ -1,17 +1,42 @@
 package com.vordead.snoozeloo.alarm.di
 
-import com.vordead.snoozeloo.alarm.data.local.AlarmDataSourceImpl
+import android.content.Context
+import androidx.room.Room
+import com.vordead.snoozeloo.alarm.data.local.LocalAlarmDataSource
+import com.vordead.snoozeloo.alarm.data.local.SnoozelooDatabase
 import com.vordead.snoozeloo.alarm.domain.AlarmDataSource
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+import kotlin.jvm.java
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class AlarmModule {
+abstract class DataSourceModule {
 
     @Binds
-    abstract fun bindAlarmDataSource(alarmDataSourceImpl: AlarmDataSourceImpl): AlarmDataSource
+    abstract fun bindAlarmDataSource(localAlarmDataSource: LocalAlarmDataSource): AlarmDataSource
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): SnoozelooDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            SnoozelooDatabase::class.java,
+            "snoozeloo_database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAlarmDao(snoozelooDatabase: SnoozelooDatabase) = snoozelooDatabase.alarmDao()
 }
